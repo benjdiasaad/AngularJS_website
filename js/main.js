fbaab0060fd735441c492ad778c45947d8766294
benjdia <cravatas90@gmail.com>
2020-11-12 21:24:56 +0100

    show data using ng-repeat and ng-controller

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var app = angular.module("myApp",[])
 
 app.controller("myCtr",function($scope){
 
-    $scope.names=['Benjdia','Omari','hussein'];
-    $scope.degree =" Master ISI";
+    $scope.friends =[{name:"saad",desc:"developper"},
+                      {name:"cristiano", desc:"player"},
+                      {name:"ayoub", desc:"artist"},
+                      {name:"skouma", desc:"romario"} ];
 })
